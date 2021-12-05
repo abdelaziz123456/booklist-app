1eb8bf71328510a801e75e417cc3a26c7eda6e79
abdelaziz mohamed <62770432+abdelaziz123456@users.noreply.github.com>
2021-12-05 21:16:53 +0200

    modify border of delete button

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ class UI{
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.isbn}</td>
-        <td><a href='#' class='btn btn-danger btn-sm delete'>X</a></td>
+        <td><a href='#' class='btn btn-danger btn-sm delete rounded-circle'>X</a></td>
         `;
 
         list.appendChild(row)
